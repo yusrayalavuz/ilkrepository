544e774ee377275bfc1b31535e7add386e8c3fae
yusraylvz <128381663+yusraylvz@users.noreply.github.com>
2024-01-05 22:55:46 +0300

    hoşgeldin

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          HOŞGELDİNİZ!
+        </p>
+        <p>
           Archi's Academy Github Kursuna Hoşgeldiniz!
         </p>
         <a
